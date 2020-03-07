4a3d6163a0abeecf8e837aaf1a9cea9ab21b9230
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-07 03:41:56 +0100

    Add fake data to FakeRepo

--- auction.ex
+++ auction.ex
@@ -20,3 +20,32 @@ defmodule Auction.Item  do
   defstruct [:id, :title, :description, :ends_at]
 
 end
+
+defmodule Auction.FakeRepo do
+  alias Auction.Item
+
+  @items [
+    %Item{
+      id: 1,
+      title: "First Item",
+      description: "Nice description.",
+      ends_at: ~N[2020-01-01 00:00:00]
+    },
+    %Item{
+      id: 2,
+      title: "Second Item",
+      description: "Functional thoughts melt my brain.",
+      ends_at: ~N[2020-01-01 00:00:00]
+    },
+    %Item{
+      id: 3,
+      title: "Third Item",
+      description: "Buy three, get three for free.",
+      ends_at: ~N[2020-01-01 00:00:00]
+    }
+  ]
+
+  def all(Item), do: @items
+end
+
+
