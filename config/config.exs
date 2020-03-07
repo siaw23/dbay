c416e9071d50a84d9c4aabef406ce9082950ab48
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-07 19:04:21 +0100

    Generate Phoenix application

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,17 @@
 # move said applications out of the umbrella.
 import Config
 
+config :auction_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :auction_web, AuctionWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "7p+gvVdUpj+Jz8ZAph1zz33onN9YlmRfLDVXpvPMVmm7l7pqGIM3x99PaZS6/Mie",
+  render_errors: [view: AuctionWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: AuctionWeb.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [signing_salt: "sf64oedb"]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -16,3 +27,15 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
