e44f17cdf55ea20f011604b24fb6e745855b8578
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-07 12:53:06 +0100

    Fix 'Auction.FakeRepo.get_by/2 is undefined or private' warning

--- auction.ex
+++ auction.ex
@@ -1,21 +1,3 @@
-defmodule Auction do
-  alias Auction.{FakeRepo, Item}
-
-  @repo FakeRepo
-
-  def list_items do
-    @repo.all(Item)
-  end
-
-  def get_item(id) do
-    @repo.get!(Item, id)
-  end
-
-  def get_item_by(attrs) do
-    @repo.get_by(Item, attrs)
-  end
-end
-
 defmodule Auction.Item  do
   defstruct [:id, :title, :description, :ends_at]
 
@@ -48,4 +30,20 @@ defmodule Auction.FakeRepo do
   def all(Item), do: @items
 end
 
+defmodule Auction do
+  alias Auction.{FakeRepo, Item}
 
+  @repo FakeRepo
+
+  def list_items do
+    @repo.all(Item)
+  end
+
+  def get_item(id) do
+    @repo.get!(Item, id)
+  end
+
+  def get_item_by(attrs) do
+    @repo.get_by(Item, attrs)
+  end
+end
