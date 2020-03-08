543afd9be2cedf2b0a903bf80641406202c072cd
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-08 11:10:29 +0100

    Add Ecto configuration

--- apps/auction_web/mix.exs
+++ apps/auction_web/mix.exs
@@ -44,6 +44,7 @@ defmodule AuctionWeb.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:phoenix_ecto, "~> 4.0"},
       {:auction, in_umbrella: true}
     ]
   end
