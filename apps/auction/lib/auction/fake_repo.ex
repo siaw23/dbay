3813518c2cf4453465ca54b7a882893c4ba94a94
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-08 12:02:05 +0100

    Point repo to a real DB

--- apps/auction/lib/auction/fake_repo.ex
+++ apps/auction/lib/auction/fake_repo.ex
@@ -1,38 +1,38 @@
-defmodule Auction.FakeRepo do
-  alias Auction.Item
+# defmodule Auction.FakeRepo do
+#   alias Auction.Item
 
-  @items [
-    %Item{
-      id: 1,
-      title: "Oculus Quest",
-      description: "A VR headset from Facebook.",
-      ends_at: ~N[2020-01-01 00:00:00]
-    },
-    %Item{
-      id: 2,
-      title: "Phoenix Web Framework",
-      description: "A nice web framework by Chris McCord.",
-      ends_at: ~N[2020-01-01 00:00:00]
-    },
-    %Item{
-      id: 3,
-      title: "Ghana",
-      description: "My beloved country.",
-      ends_at: ~N[2020-01-01 00:00:00]
-    }
-  ]
+#   @items [
+#     %Item{
+#       id: 1,
+#       title: "Oculus Quest",
+#       description: "A VR headset from Facebook.",
+#       ends_at: ~N[2020-01-01 00:00:00]
+#     },
+#     %Item{
+#       id: 2,
+#       title: "Phoenix Web Framework",
+#       description: "A nice web framework by Chris McCord.",
+#       ends_at: ~N[2020-01-01 00:00:00]
+#     },
+#     %Item{
+#       id: 3,
+#       title: "Ghana",
+#       description: "My beloved country.",
+#       ends_at: ~N[2020-01-01 00:00:00]
+#     }
+#   ]
 
-  def all(Item), do: @items
+#   def all(Item), do: @items
 
-  def get!(Item, id) do
-    Enum.find(@items, fn(item) -> item.id === id end)
-  end
+#   def get!(Item, id) do
+#     Enum.find(@items, fn(item) -> item.id === id end)
+#   end
 
-  def get_by(Item, attrs) do
-    Enum.find(@items, fn(item) ->
-      Enum.all?(Map.keys(attrs), fn(key) ->
-        Map.get(item, key) === attrs[key]
-      end)
-    end)
-  end
-end
+#   def get_by(Item, attrs) do
+#     Enum.find(@items, fn(item) ->
+#       Enum.all?(Map.keys(attrs), fn(key) ->
+#         Map.get(item, key) === attrs[key]
+#       end)
+#     end)
+#   end
+# end
