7e9725087e768d7ee651c2c136157fc8e886998e
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-09 16:05:14 +0100

    Add User changesets and absract password hashing to its own module

--- apps/auction/lib/auction/user.ex
+++ apps/auction/lib/auction/user.ex
@@ -1,4 +1,5 @@
 defmodule Auction.User do
+  import Ecto.Changeset
   use Ecto.Schema
 
   schema "users" do
@@ -9,4 +10,29 @@ defmodule Auction.User do
 
     timestamps()
   end
+
+  def changeset(user, params \\ %{}) do
+    user
+    |> cast(params, [:username, :email_address])
+    |> validate_required([:username, :email_address, :hashed_password])
+    |> validate_length(:username, min: 3)
+    |> unique_constraint(:username)
+  end
+
+  def changeset_with_password(user, params \\ %{}) do
+    user
+    |> cast(params, [:password])
+    |> validate_required(:password)
+    |> validate_length(:password, min: 5)
+    |> validate_confirmation(:password, required: true)
+    |> hash_password()
+    |> changeset(params)
+  end
+
+  defp hash_password(%Ecto.Changeset{changes: %{password: password}} = changeset) do
+    changeset
+    |> put_change(:hashed_password, Auction.Password.hash(password))
+  end
+
+  defp hash_password(changeset), do: changeset
 end
