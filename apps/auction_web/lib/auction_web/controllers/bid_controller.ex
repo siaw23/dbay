56f55e10189e417a5870a2fe6906fb426c452961
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-10 17:24:17 +0100

    Add Bid controller

---
+++ apps/auction_web/lib/auction_web/controllers/bid_controller.ex
@@ -0,0 +1,15 @@
+defmacro AuctionWeb.BidController do
+  user AuctionWeb, :BidController
+  plug :require_logged_in_user
+
+  def create(conn, %{"bid" => %{"amount" => amount}, "item_id" => item_id}) do
+    user_id = conn.assigns.current_user.user_id
+
+    case Auction.insert_bid(%{amount: amount, item_id: item_id, user_id: user_id}) do
+      {:ok, bid} -> redirect(conn, to: Routes.item_path(conn, :show, bid.item_id))
+      {:error, bid} ->
+        item = Auction.get_item(item_id)
+        render(conn, AuctionWeb.ItemView, "show.html", item: item, bid: bid)
+    end
+  end
+end
