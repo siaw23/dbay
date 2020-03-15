ef81b3ddcae6204b11892aa20dbbea980beb8ac0
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-16 00:25:34 +0100

    Refactor ItemView

--- apps/auction_web/lib/auction_web/views/api/item_view.ex
+++ apps/auction_web/lib/auction_web/views/api/item_view.ex
@@ -2,14 +2,28 @@ defmodule AuctionWeb.Api.ItemView do
   use AuctionWeb, :view
 
   def render("show.json", %{item: item}) do
-    %{
-      data: %{
-        type: "item",
-        id: item.id,
-        title: item.title,
-        description: item.description,
-        ends_at: item.ends_at
-      }
-    }
+    %{data: render_one(item, __MODULE__, "item.json")}
   end
+
+  def render("item.json", %{item: item}) do
+    %{
+      type: "item",
+      id: item.id,
+      title: item.title,
+      description: item.description,
+      ends_at: item.ends_at
+      }
+  end
+
+  # def render("show.json", %{item: item}) do
+  #   %{
+  #     data: %{
+  #       type: "item",
+  #       id: item.id,
+  #       title: item.title,
+  #       description: item.description,
+  #       ends_at: item.ends_at
+  #     }
+  #   }
+  # end
 end
