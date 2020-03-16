0b8688952f6e73941145ce60b9c72eb42c133f78
Emmanuel Hayford <siawmensah@gmail.com>
2020-03-16 01:36:22 +0100

    Complete API

--- apps/auction_web/lib/auction_web/views/api/item_view.ex
+++ apps/auction_web/lib/auction_web/views/api/item_view.ex
@@ -6,7 +6,18 @@ defmodule AuctionWeb.Api.ItemView do
   end
 
   def render("show.json", %{item: item}) do
-    %{data: render_one(item, __MODULE__, "item.json")}
+    %{data: render_one(item, __MODULE__, "item_with_bids.json")}
+  end
+
+  def render("item_with_bids.json", %{item: item}) do
+    %{
+      type: "item",
+      id: item.id,
+      title: item.title,
+      description: item.description,
+      ends_at: item.ends_at,
+      bids: render_many(item.bids, AuctionWeb.Api.BidView, "bid.json")
+    }
   end
 
   def render("item.json", %{item: item}) do
@@ -16,18 +27,6 @@ defmodule AuctionWeb.Api.ItemView do
       title: item.title,
       description: item.description,
       ends_at: item.ends_at
-      }
+    }
   end
-
-  # def render("show.json", %{item: item}) do
-  #   %{
-  #     data: %{
-  #       type: "item",
-  #       id: item.id,
-  #       title: item.title,
-  #       description: item.description,
-  #       ends_at: item.ends_at
-  #     }
-  #   }
-  # end
 end
